5d6d3adcf0c088487acbdc8c9500bd7cde7a70d0
aditya-verinite <aditya.suryawanshi@verinite.com>
2024-01-22 15:02:34 +0530

    frontend

---
+++ frontend/src/app/file-upload/file-upload.component.ts
@@ -0,0 +1,45 @@
+import { HttpClient } from '@angular/common/http';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-file-upload',
+  templateUrl: './file-upload.component.html',
+  styleUrls: ['./file-upload.component.css']
+})
+export class FileUploadComponent {
+
+  constructor(private http:HttpClient){
+
+  }
+  flag = true //file is not being upload 
+  file:any
+  url="http://localhost:8080/user/upload";
+
+
+  selectFile(event:any){
+   
+
+    this.file=event.target.files[0];
+
+    console.log(this.file);
+  }
+
+  uploadFile(){
+    let formData = new FormData;
+    formData.append("file",this.file);
+    this.flag=false;
+    this.http.post(this.url,formData).subscribe(
+    (data:any )=>{
+      //success
+      console.log(data);
+      this.flag=true;
+      alert(data.message);
+    },(error)=>{
+      //errror 
+      this.flag=true;
+      console.log(error);
+      alert(error);
+    })
+    
+  }
+}
